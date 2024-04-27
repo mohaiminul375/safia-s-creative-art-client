e2d12874b82dc204f51878612e00acb35c2f8bf0
Mohaiminul375 <mohaiminul375@gmail.com>
2024-04-27 19:21:40 +0600

    dataadded category data card

--- src/components/pages/CategoryData/CategoryData.jsx
+++ src/components/pages/CategoryData/CategoryData.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+
+import { useLoaderData, useParams } from 'react-router-dom';
+import CategoryDataCard from '../CategoryDataCard/CategoryDataCard';
 
 const CategoryData = () => {
-    const data = useLoaderData()
-    console.log(data);
+    const artAndCraftData = useLoaderData()
+    // console.log(data);
+    const {category} = useParams();
+    console.log('params',category)
     return (
         <div>
-            <h2>CategoryDataHere</h2>
+            <h2 className='mt-12 text-center text-5xl font-bold font-rancho'>Art&Craft of: {category} Category</h2>
+            <div className='grid md:grid-cols-3 gap-5 mt-10'>
+              {
+               artAndCraftData.map(data=><CategoryDataCard key={data._id}data={data}></CategoryDataCard>)
+              }
+            </div>
         </div>
     );
 };
---
+++ src/components/pages/CategoryDataCard/CategoryDataCard.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+
+
+const CategoryDataCard = ({data}) => {
+
+    const {
+        _id,
+        item_name,
+        subcategory_Name,
+        photo,
+        price,
+        rating,
+        stockStatus,
+        processing_time
+      } = data;
+    return (
+        <div className="max-w-lg p-4 shadow-md dark:bg-gray-50 dark:text-gray-800">
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <img
+              src={photo}
+              alt=""
+              className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
+            />
+          </div>
+          <div className="space-y-2">
+            <a rel="noopener noreferrer" href="#" className="block">
+              <h3 className="text-xl font-semibold dark:text-[#001220] hover:underline tooltip tooltip-bottom" data-tip={item_name}>
+                {item_name.length>30? item_name.slice(0,20)+"...": item_name}
+              </h3>
+            </a>
+  
+            <p className="dark:text-gray-600 text-lg">
+              <span className="font-bold text-[#001220]">Category:</span>{" "}
+              {subcategory_Name}
+            </p>
+            <p className=" dark:text-gray-600 text-xl">
+              <span className="font-bold text-[#001220]">Stock:</span>{" "}
+              {stockStatus}
+            </p>
+  
+            <div className="flex justify-between">
+              <p className="text-base font-bold dark:text-gray-600 hover:text-[#001220]">
+                <span className="text-[#001220]">Price:</span> ${price}
+              </p>
+              <p className="text-base font-bold dark:text-gray-600 hover:text-[#001220]">
+                <span className="text-[#001220]">Rating:</span> {rating}
+              </p>
+            </div>
+            <div>
+            <p className="text-base font-bold dark:text-gray-600 hover:text-[#001220]">
+                <span className="text-[#001220]">Processing time:</span> {processing_time}
+              </p>
+            </div>
+            <Link to={`/Art&CraftDetails/${_id}`}>
+              <button className="mt-5 text-center w-full py-2 border border-[#001220] hover:bg-[#001220] hover:text-white hover:transition-opacity">
+                View Details
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default CategoryDataCard;
